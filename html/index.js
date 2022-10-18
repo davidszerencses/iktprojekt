f330e03b4313652cbe417e9778b09f7deecc6bc9
LilHacker09 <100405931+StringSlinger097@users.noreply.github.com>
2022-10-18 16:07:33 +0200

    10.18-1

---
+++ html/index.js
@@ -0,0 +1,18 @@
+    function igen1() {
+      alert('nemmegyhehe, nem tudom mit vártál haha')
+    };
+    function igen2() {
+      alert('mégmindignemmegyhehe, nem tudom mit vártál haha')
+    };
+    function igen3() {
+      alert('Megint megpróbáltad?, szégyen')
+    };
+    function igen4() {
+      alert('Ismét próbálkozol?, nagy bajok vannak!')
+    };
+    function igen5() {
+      alert('Te tényleg nem tudod abbahagyni a gomboknak a nyomkodását?, nagyobb bajok vannak mint hittem')
+    };
+    function igen3() {
+      alert('Bruh! Remélem ez már az utolsó, szégyen')
+    };
